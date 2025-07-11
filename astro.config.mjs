04b4d96a05b4554295637cb81bd814f67e43eca6
gioxoay <nguyen.dainghia@gmail.com>
2025-07-11 16:21:31 +0700

    Commit #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://data-vn.github.io'
+    site: 'https://data-vn.github.io',
+    integrations: [react()]
 });
